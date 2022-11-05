4add626dc241371507bc73bb4dcb1a20f1bb88ad
Avyaan Verma <avyaanverma2022@vitbhopal.ac.in>
2022-11-05 22:43:24 +0530

    Hello World Documentation

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
 int main(){
-    std::cout<<"Hello World";
+    std::cout<<"Hello World"<< std::endl;//endline will flush the output buffer
+    std::cout<<"Hope you are not reading this!"<<'\n';// adding a new line character  does the same thing and is better performance wise
 return 0;
 }
